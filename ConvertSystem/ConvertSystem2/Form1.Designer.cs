6084c15d1457b6b5902dd45f5301f3aebcb2b661
keunaa88 <keunaa88@gmail.com>
2017-03-24 15:20:25 +0800

    version2_170324

--- ConvertSystem/ConvertSystem2/Form1.Designer.cs
+++ ConvertSystem/ConvertSystem2/Form1.Designer.cs
@@ -40,100 +40,161 @@
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 117);
+            this.textBox1.Location = new System.Drawing.Point(141, 134);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(67, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(29, 143);
+            this.textBox2.Location = new System.Drawing.Point(141, 160);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(67, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(29, 169);
+            this.textBox3.Location = new System.Drawing.Point(141, 186);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(66, 20);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(29, 195);
+            this.textBox4.Location = new System.Drawing.Point(141, 212);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(66, 20);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(29, 221);
+            this.textBox5.Location = new System.Drawing.Point(141, 238);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.Size = new System.Drawing.Size(66, 20);
             this.textBox5.TabIndex = 4;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(127, 81);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Convert Centimetres to Inches",
+            "Convert Metres to Feet ",
+            "Convert Celsius to Fahrenheit",
+            "Convert Centimetres to Feet",
+            "Convert Kilometres to Miles"});
+            this.comboBox1.Location = new System.Drawing.Point(102, 95);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(135, 21);
+            this.comboBox1.Size = new System.Drawing.Size(171, 21);
             this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndex = 0;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(162, 221);
+            this.textBox6.Location = new System.Drawing.Point(287, 135);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(66, 20);
             this.textBox6.TabIndex = 10;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(162, 195);
+            this.textBox7.Location = new System.Drawing.Point(287, 161);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(66, 20);
             this.textBox7.TabIndex = 9;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(162, 169);
+            this.textBox8.Location = new System.Drawing.Point(288, 186);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(66, 20);
             this.textBox8.TabIndex = 8;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(162, 143);
+            this.textBox9.Location = new System.Drawing.Point(288, 212);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(66, 20);
             this.textBox9.TabIndex = 7;
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(162, 117);
+            this.textBox10.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox10.Location = new System.Drawing.Point(288, 238);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(66, 20);
             this.textBox10.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 46);
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.Size = new System.Drawing.Size(137, 20);
             this.label1.TabIndex = 11;
             this.label1.Text = "Convert System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Convert Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Input";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(238, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 274);
+            this.ClientSize = new System.Drawing.Size(381, 300);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox7);
@@ -147,7 +208,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Convert System";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,6 +228,10 @@
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ConvertSystem/ConvertSystem2/Form1.cs
+++ ConvertSystem/ConvertSystem2/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,8 +16,64 @@ namespace ConvertSystem2
         public Form1()
         {
             InitializeComponent();
+            
         }
 
-     
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+            double [] input = new double[5];
+            double [] output = new double[5];
+
+            //convert input values to integer
+            double.TryParse(textBox1.Text, out input[0]);
+            double.TryParse(textBox2.Text, out input[1]);
+            double.TryParse(textBox3.Text, out input[2]);
+            double.TryParse(textBox4.Text, out input[3]);
+            double.TryParse(textBox5.Text, out input[4]);
+
+            
+            int convertType = comboBox1.SelectedIndex;
+            double multipleNum = 0;
+            switch (convertType)
+            {
+                case 0:
+                    //Convert Centimetres to Inches
+                    multipleNum = 0.393701;
+                    break;
+                case 1:
+                    //Convert Metres to Feet
+                    multipleNum = 3.28084;
+                    break;
+                case 2:
+                    //Convert Celsius to Fahrenheit
+                    multipleNum = 32;
+                    break;
+                case 3:
+                    //Convert Centimetres to Feet
+                    multipleNum = 0.0328084;
+                    break;
+                case 4:
+                    //Convert Kilometres to Miles
+                    multipleNum = 0.621371;
+                    break;
+                default:
+                    break;
+            }
+
+            for(int i=0; i<5; i++)
+            {
+                output[i] = input[i] * multipleNum;
+            }
+            
+         
+            textBox6.Text = output[0].ToString();
+            textBox7.Text = output[1].ToString();
+            textBox8.Text = output[2].ToString();
+            textBox9.Text = output[3].ToString();
+            textBox10.Text = output[4].ToString();
+
+        }
+
     }
 }
